68debacee0129ae1bd0a70c674684892b84ac705
Kamran -Waris <mianwarissb@gmail.com>
2022-07-15 15:46:49 +0500

    web routes

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -31,7 +31,15 @@ window.Startloader = (val) => {
   }
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.baseURL = window.location.origin + '/api/';
+axios.defaults.baseURL = window.location.origin;
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
