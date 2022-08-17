434597e63c5fde07679f4bdf2eda5184eac8f228
Ashar4774 <ashar.muhammad74@gmail.com>
2022-08-17 18:35:09 +0500

    nothing much

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'host' => 'localhost:8000',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
